b24f10dc26f2c428484945a4aeca76fbc396e7f0
karisma70 <karisma70kr@gmail.com>
2017-03-29 15:46:23 +0900

    Chapter10 디렉토리 생성 및 os_info.js 파일 추가 업로드

---
+++ Chapter10/os_info.js
@@ -0,0 +1,10 @@
+/**
+ * Created by david on 2017-03-29.
+ */
+varos = require('os');
+
+console.log("tmpdir :\t" + varos.tmpdir() );
+
+console.log("platform : \t" + varos.platform() );
+
+console.log("end,,,,,,  \t" );
