839754b2d406e245c6120873f05ac5ccac79a5c0
David Lee <karisma70kr@gmail.com>
2017-04-13 10:47:24 +0900

    로컬 nodejs로 구동할때와 원격 nodejs로 구동할 경우 소스에 반영

--- Chapter7/http_client_static.js
+++ Chapter7/http_client_static.js
@@ -4,9 +4,11 @@
 
 var http = require('http');
 var options = {
-    hostname : '13.124.51.117',
+    hostname : '13.124.86.217',      // 윈도우즈에서 원격으로 서버에 접속하고자 할 경우
+    // hostname : '127.0.0.1',        // or 'localhost',      // 서버가 구동하는 시스템의 로컬 클라이언트에서 로컬 서버로 접속하고자 할 경우
     port : 8081,
     path: '/jquery_study2.html'
+    // path: '/hello.html'
 };
 
 function handleResponse( response ){
